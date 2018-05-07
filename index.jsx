e23409c0f877b3a4e0572649f42b6355784afb8c
Thomas Bertet <thomas.bertet@mirakl.com>
2018-05-07 15:36:28 +0200

    demo project with missing suggested imports

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Bob extends Component {
+    render() {
+        return <div>
+            /* Try the menu "Intentions & Quick-fixes  (Alt+Enter) on the "Button" component. */
+            <Button>Hello</Button>
+        </div>
+    }
+}
